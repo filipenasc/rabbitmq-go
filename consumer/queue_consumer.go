c26d144c765c27cd43ddb89aa23bd0f59d1d2eea
Filipe Nascimento <filipemateusn@gmail.com>
2018-05-30 22:32:26 -0300

    Add production url

--- consumer/queue_consumer.go
+++ consumer/queue_consumer.go
@@ -1,6 +1,7 @@
 package main
 
 import(
+	"os"
 	"log"
   "./dispatcher"
 
@@ -14,7 +15,7 @@ func failOnError(err error, msg string) {
 }
 
 func main() {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	conn, err := amqp.Dial(os.Getenv("RABBITMQ_URL"))
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
--- producer/send.go
+++ producer/send.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"fmt"
+	"os"
 
 	"github.com/streadway/amqp"
 )
@@ -20,7 +21,7 @@ func failOnError(err error, msg string) {
 }
 
 func main() {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	conn, err := amqp.Dial(os.Getenv("RABBITMQ_URL"))
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
